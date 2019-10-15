1005cacc45cfbaab66f5f4209d71f1771783aa96
Justin Gent <justinbgent@gmail.com>
2019-10-15 14:14:09 -0600

    Add loadImageUrl function to extensions.

--- M02/ExtensionsProject/app/src/main/java/com/schoolwork/extensionsproject/Extensions.kt
+++ M02/ExtensionsProject/app/src/main/java/com/schoolwork/extensionsproject/Extensions.kt
@@ -1,2 +1,9 @@
 package com.schoolwork.extensionsproject
 
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+fun Context.loadImageUrl(url: String, imageView: ImageView){
+    Glide.with(this).load(url).into(imageView)
+}
