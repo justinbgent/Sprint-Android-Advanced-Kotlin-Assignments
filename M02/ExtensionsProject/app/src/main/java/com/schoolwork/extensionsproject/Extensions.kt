e3840f9ba3b4f0e399203404a9cffc9afd17f10d
Justin Gent <justinbgent@gmail.com>
2019-10-15 16:05:05 -0600

    Add listener to loadImageUrl extension function

--- M02/ExtensionsProject/app/src/main/java/com/schoolwork/extensionsproject/Extensions.kt
+++ M02/ExtensionsProject/app/src/main/java/com/schoolwork/extensionsproject/Extensions.kt
@@ -4,11 +4,16 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.widget.ImageView
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 fun Context.notification(title: String = "Notification", message:String, notifyId: Int, channelId: String = ""){
     val notificationBuilder = NotificationCompat.Builder(this, channelId)
@@ -30,5 +35,26 @@ fun Context.notification(title: String = "Notification", message:String, notifyI
 }
 
 fun ImageView.loadImageUrl(url: String){
-    Glide.with(this.context).load(url).into(this)
+    Glide.with(this.context).load(url)
+        .listener(object : RequestListener<Drawable>{
+            override fun onLoadFailed(e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                this@loadImageUrl.setImageDrawable(ContextCompat
+                    .getDrawable(this@loadImageUrl.context, R.drawable.ic_launcher_foreground))
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                return false
+            }
+        }).into(this)
 }
