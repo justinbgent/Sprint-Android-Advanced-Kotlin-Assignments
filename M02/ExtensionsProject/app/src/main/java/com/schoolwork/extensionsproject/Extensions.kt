2c664e34a774e3538c8485ad990cfadd1c6a1199
Justin Gent <justinbgent@gmail.com>
2019-10-15 14:50:06 -0600

    Add extension function for notifications.

--- M02/ExtensionsProject/app/src/main/java/com/schoolwork/extensionsproject/Extensions.kt
+++ M02/ExtensionsProject/app/src/main/java/com/schoolwork/extensionsproject/Extensions.kt
@@ -1,8 +1,33 @@
 package com.schoolwork.extensionsproject
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import android.widget.ImageView
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+
+fun Context.notification(title: String = "Notification", message:String, notifyId: Int, channelId: String = ""){
+    val notificationBuilder = NotificationCompat.Builder(this, channelId)
+    notificationBuilder.setContentTitle(title)
+        .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+        .setContentText(message)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        .setDefaults(Notification.DEFAULT_ALL)
+
+    val notifManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        val name = "Default Notifications Channel"
+        val notifChannel = NotificationChannel(channelId, name, NotificationManager.IMPORTANCE_DEFAULT)
+        notifChannel.description = "Where you find default notifs"
+        notifManager.createNotificationChannel(notifChannel)
+    }
+    notifManager.notify(notifyId, notificationBuilder.build())
+}
 
 fun Context.loadImageUrl(url: String, imageView: ImageView){
     Glide.with(this).load(url).into(imageView)
