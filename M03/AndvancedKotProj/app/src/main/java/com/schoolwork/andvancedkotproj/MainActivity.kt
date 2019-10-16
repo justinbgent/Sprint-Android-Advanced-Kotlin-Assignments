96bf1774e76ea13fd25d5ef642216db644df081f
Justin Gent <justinbgent@gmail.com>
2019-10-16 14:10:21 -0600

    Create recyclerView to use.

--- M03/AndvancedKotProj/app/src/main/java/com/schoolwork/andvancedkotproj/MainActivity.kt
+++ M03/AndvancedKotProj/app/src/main/java/com/schoolwork/andvancedkotproj/MainActivity.kt
@@ -2,11 +2,36 @@ package com.schoolwork.andvancedkotproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recycler_item.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+
+class Recycler(private var list: MutableList<Genres>): RecyclerView.Adapter<Recycler.ViewHolder>(){
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val txtView: TextView = view.txt_view
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout. , parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = list[position]
+        holder.txtView.text = item.name
     }
 }
