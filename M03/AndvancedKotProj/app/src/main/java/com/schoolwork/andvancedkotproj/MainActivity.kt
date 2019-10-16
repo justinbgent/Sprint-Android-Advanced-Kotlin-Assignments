b22dbc3e30adf41502426e3318fb653de71cb210
Justin Gent <justinbgent@gmail.com>
2019-10-16 15:28:49 -0600

    Options menu will display and is functional

--- M03/AndvancedKotProj/app/src/main/java/com/schoolwork/andvancedkotproj/MainActivity.kt
+++ M03/AndvancedKotProj/app/src/main/java/com/schoolwork/andvancedkotproj/MainActivity.kt
@@ -2,6 +2,8 @@ package com.schoolwork.andvancedkotproj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +17,30 @@ class MainActivity : AppCompatActivity() {
         EDM("Room for Happiness"),
         EDM("Angel On My Shoulder"),
         EDM("4AM"),
+        EDM("Atmosphere"),
+        EDM("Sad Machine"),
+        EDM("Room for Happiness"),
+        EDM("Angel On My Shoulder"),
+        EDM("4AM"),
+        EDM("Atmosphere"),
+        EDM("Sad Machine"),
+        EDM("Room for Happiness"),
+        EDM("Angel On My Shoulder"),
+        EDM("4AM"),
         EDM("Atmosphere")
     )
 
     var drumAndBaseList = mutableListOf<Song>(
+        DrumAndBase("Freefall"),
+        DrumAndBase("Legacy"),
+        DrumAndBase("Tearing Me Apart"),
+        DrumAndBase("Just A Thought"),
+        DrumAndBase("Night & Day"),
+        DrumAndBase("Freefall"),
+        DrumAndBase("Legacy"),
+        DrumAndBase("Tearing Me Apart"),
+        DrumAndBase("Just A Thought"),
+        DrumAndBase("Night & Day"),
         DrumAndBase("Freefall"),
         DrumAndBase("Legacy"),
         DrumAndBase("Tearing Me Apart"),
@@ -31,6 +53,16 @@ class MainActivity : AppCompatActivity() {
         Dubstep("Flight"),
         Dubstep("Frame of Mind"),
         Dubstep("Hey Now"),
+        Dubstep("It's Cool"),
+        Dubstep("Finale"),
+        Dubstep("Flight"),
+        Dubstep("Frame of Mind"),
+        Dubstep("Hey Now"),
+        Dubstep("It's Cool"),
+        Dubstep("Finale"),
+        Dubstep("Flight"),
+        Dubstep("Frame of Mind"),
+        Dubstep("Hey Now"),
         Dubstep("It's Cool")
     )
 
@@ -39,5 +71,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerView.startRecyclerView(dubstepList)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_default, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.title){
+            "EDM" -> recyclerView.startRecyclerView(edmList)
+            "Dubstep" -> recyclerView.startRecyclerView(dubstepList)
+            "Drum and Base" -> recyclerView.startRecyclerView(drumAndBaseList)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
